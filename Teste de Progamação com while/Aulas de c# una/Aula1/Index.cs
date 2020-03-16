cf1b3cd4ca6922a4175027f0539b82c990ec259b
Alexsiks <55172363+alexsiks@users.noreply.github.com>
2020-03-16 04:01:58 -0300

    Add files via upload

---
+++ Teste de Progamação com while/Aulas de c# una/Aula1/Index.cs
@@ -0,0 +1,13 @@
+//Biblioteca do Sistema//
+
+using System;
+
+class Principal {
+    static void Main () {
+
+        Console.Write("Bem vindo ao sistema de idade:");
+//
+
+    }
+
+}
---
+++ Teste de Progamação com while/Aulas de c# una/Aula3/Aula3.cs
@@ -0,0 +1,15 @@
+using System;
+class Aula3 {
+
+    static void Main () {
+
+            //Tipos de variaveis//
+
+            int num = 0;
+            char letra = '8';
+            float valor = 5.3f;
+            string nome = "Bruno";
+
+            var aux = "Cfb Cursos";
+
+        } {
